999a730c5659443bb1078f7061a3209d96af604f
mhammami <Michael.Hammami@infotrack.co.uk>
2019-09-26 16:05:17 +0100

    AcceptNewCustomerTermsVersion

--- AMLv4_API/Aml_v4_API/CustomerTermsRevisions/AcceptNewCustomerTermsVersion.cs
+++ AMLv4_API/Aml_v4_API/CustomerTermsRevisions/AcceptNewCustomerTermsVersion.cs
@@ -1,25 +1,38 @@
-﻿namespace AMLv4_API.Aml_v4_API
+﻿using Newtonsoft.Json;
+
+namespace AMLv4_API.Aml_v4_API
 {
     /// <summary>
     /// Accept new version of customer terms. The version stored for the customer will be updated
     /// to the latest.
     /// </summary>
-    public partial class AcceptNewCustomerTermsVersionRequest
+    public class AcceptNewCustomerTermsVersionRequest
     {
-        public string uri { get; set; }
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
     }
 
     public class AcceptNewCustomerTermsVersionResponse
     {
-        public string error { get; set; }
-        public string uri { get; set; }
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        [JsonProperty("DateAccepted")]
         public string DateAccepted { get; set; }
+
+        [JsonProperty("TermsVersion")]
         public string TermsVersion { get; set; }
+
+        [JsonProperty("TermsVersionViewed")]
         public string TermsVersionViewed { get; set; }
+
+        [JsonProperty("AmlTermsVersionCurrent")]
         public string AmlTermsVersionCurrent { get; set; }
+
+        [JsonProperty("AmlTermsVersionNew")]
         public string AmlTermsVersionNew { get; set; }
     }
-
-
-
 }
